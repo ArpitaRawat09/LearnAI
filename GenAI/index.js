dac0771135710f14fa9e143c0085f9023a7df008
Arpita Rawat <arpitarawat381@gmail.com>
2026-09-23 18:58:01 +0530

    feat: add readline dependency and implement prompt input in index.js

--- GenAI/index.js
+++ GenAI/index.js
@@ -1,24 +1,33 @@
 import { ChatMistralAI } from "@langchain/mistralai";
 import { config } from "dotenv";
+import rl from "readline/promises";
 
 config();
 
-const model = new ChatMistralAI({
-  model: "ministral-3b-latest",
-  apiKey: process.env.MISTRAL_API_KEY,
+const readline = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+
+const usePrompt = await readline.question("Enter your Prompt:")
+console.log(usePrompt);
+readline.close();
+
+
+// const model = new ChatMistralAI({
+//   model: "ministral-3b-latest",
+//   apiKey: process.env.MISTRAL_API_KEY,
+// });
 
 // Response will come  at the same time------->
 // const response = await model.invoke("provide factorial code in javascript.");
 
 // console.log(response.text);
 
-
-
 // Response comes in word to word formate---------->
-const stream = await model.stream("give prime number code in iterative in js ")
+// const stream = await model.stream("give prime number code in iterative in js ")
 
-
-for await( const chunk of stream){
-    process.stdout.write(chunk.text)
-}
+// for await( const chunk of stream){
+//     process.stdout.write(chunk.text)
+// }
